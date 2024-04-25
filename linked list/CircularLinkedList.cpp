a964a8d38548727aa659a6d63213cfc79106ecef
nouraldein66 <111093819+nouraldein66@users.noreply.github.com>
2024-04-25 19:37:02 +0200

    Update CircularLinkedList.cpp

--- linked list/CircularLinkedList.cpp
+++ linked list/CircularLinkedList.cpp
@@ -206,3 +206,104 @@ public:
     }
 
 };
+
+int main(){
+
+    CircularLinkedList<int> l1;
+    cout << "Insert At Head  : ";
+    l1.insertAtHead(1);
+    l1.insertAtHead(2);
+    l1.insertAtHead(3);
+    l1.print();
+    cout << endl << "------------------------------------------------------------\n";
+
+
+    CircularLinkedList<int>l2;
+    cout << "Insert At Tail  : ";
+    l2.insertAtTail(1);
+    l2.insertAtTail(2);
+    l2.insertAtTail(3);
+    l2.print();
+    cout << endl << "------------------------------------------------------------\n";
+
+
+    cout << "Insert At specific position 2 put 5 in the prev ex  :  ";
+    l2.InsertAt(5,2);
+    l2.print();
+    cout << endl << "------------------------------------------------------------\n";
+
+
+    cout << "deleting  the head from the prev ex  :  ";
+    l2.removeAtHead();
+    l2.print();
+    cout << endl << "------------------------------------------------------------\n";
+
+
+    cout << "deleting the tail from the prev ex  :  ";
+    l2.removeAtTail();
+    l2.print();
+    cout << endl << "------------------------------------------------------------\n";
+
+
+    l2.insertAtHead(1);
+    l2.insertAtHead(2);
+    l2.insertAtHead(10);
+    cout << "deleting an elemnt at indx 3 : \n";
+    l2.print();
+    cout << endl;
+    l2.removeAt(3);
+    l2.print();
+    cout << endl << "------------------------------------------------------------\n";
+
+
+    cout << "retrive the element at indx 3 from the prev ex : ";
+    cout << l2.retrieveAt(3);
+    cout << endl << "------------------------------------------------------------\n";
+
+
+    cout << "Replace element at pos 2  with 100 : \n";
+    l2.print();
+    cout << endl;
+    l2.replaceAt(100, 2 );
+    l2.print();
+    cout << endl << "------------------------------------------------------------\n";
+
+    cout << "check if the element is already in the linked list 100 : ";
+    if ( l2.isExist(100)){
+        cout << "yes\n";
+    }
+    else {
+        cout << "NO \n";
+    }
+    cout << endl << "------------------------------------------------------------\n";
+
+    cout << "check if the element at specific indx equal a spcific value  100 at 2  : ";
+    if ( l2.isItemAtEqual(100, 2 ))
+        cout << "YES\n";
+    else cout << "NO\n";
+    cout << endl << "------------------------------------------------------------\n";
+
+    cout << "swap two nodes with each other at indx 1 and 2 : \n";
+    l2.print();
+    cout << endl;
+    l2.swap(1,2);
+    l2.print();
+    cout << endl << "------------------------------------------------------------\n";
+
+    cout << "check if the list is empty or not : ";
+    if ( l2.isEmpty()){
+        cout << "the linkedList is empty \n";
+    }
+    else {
+        cout << "The linkedList NOt empty\n";
+    }
+    cout << endl << "------------------------------------------------------------\n";
+
+    cout <<"The size of the linked list :";
+    cout << l2.linkedListSize()<<endl;
+    cout << endl << "------------------------------------------------------------\n";
+
+    cout <<"Clear the linked list , trying to print it after clear function : ";
+    l2.clear();
+    l2.print();
+}
